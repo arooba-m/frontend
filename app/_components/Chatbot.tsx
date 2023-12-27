d2f174c31ef1ad288d314401e5d14bf9a5491e78
FatimaBadar <fatimabadarq@gmail.com>
2023-12-28 03:42:20 +0500

    CHATBOT WORKING

--- app/_components/Chatbot.tsx
+++ app/_components/Chatbot.tsx
@@ -1,5 +1,4 @@
-// components/Chatbot.tsx
-"use client"
+'use client';
 import React, { useEffect } from 'react';
 
 interface ChatbotProps {
@@ -8,23 +7,28 @@ interface ChatbotProps {
 }
 
 const Chatbot: React.FC<ChatbotProps> = ({ chatbotId, domain }) => {
+
   useEffect(() => {
-    const script = document.createElement('script');
-    script.innerHTML = `
+    // Set up the configuration object
+    const configScript = document.createElement('script');
+    configScript.setAttribute('type', 'text/javascript');
+    configScript.innerHTML = `
       window.embeddedChatbotConfig = {
         chatbotId: "${chatbotId}",
         domain: "${domain}"
       };
     `;
-    document.head.appendChild(script);
+    document.head.appendChild(configScript);
 
+    // Load the external script
     const chatbotScript = document.createElement('script');
     chatbotScript.src = 'https://www.chatbase.co/embed.min.js';
     chatbotScript.defer = true;
     document.head.appendChild(chatbotScript);
 
+    // Clean up function to remove the script when the component is unmounted
     return () => {
-      document.head.removeChild(script);
+      document.head.removeChild(configScript);
       document.head.removeChild(chatbotScript);
     };
   }, [chatbotId, domain]);
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Script from 'next/script';
 import Head from 'next/head';
 // import { Toast } from 'primereact/toast';
 const inter = Inter({ subsets: ['latin'] });
+import Chatbot from './_components/Chatbot';
 
 export const metadata: Metadata = {
   title: 'OneClicks',
@@ -22,10 +23,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           defer
           crossOrigin="anonymous"
           src="https://connect.facebook.net/en_US/sdk.js"
-        />
+        /> 
       </Head>
       <body className={inter.className}>
-        {/* <Toast /> */}
+        <Chatbot chatbotId="qgRc6KZSbQAPJObsnYrPL" domain="www.chatbase.co"  />
         {children}
       </body>
     </html>
