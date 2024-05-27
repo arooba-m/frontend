82a36b22adce3f1429b8a37ccd397fa2287c8d96
Arooba Moin <127647798+arooba-m@users.noreply.github.com>
2024-05-27 13:48:17 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,18 @@ const nextConfig = {
         // your project has type errors.
         // !! WARN !!
         ignoreBuildErrors: true,
-      }
-}
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.node = {
+                global: true,
+            };
+            config.resolve.fallback = {
+                performance: require.resolve('perf_hooks'),
+            };
+        }
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
